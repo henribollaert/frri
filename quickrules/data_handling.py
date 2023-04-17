cd719cd0cf11699beb8ff11dc12ed629f289bad9
hebollae <Henri.Bollaert@UGent.be>
2023-04-17 14:51:47 +0200

    added balanced accuracy

--- quickrules/data_handling.py
+++ quickrules/data_handling.py
@@ -56,7 +56,6 @@ def skip(name: str, include: Optional[list[str]], exclude: Optional[list[str]]):
     return (exclude is not None and name in exclude) or (include is not None and name not in include)
 
 
-# todo finish, also print info and rules!
 def test_save(
         model: RuleInductionModel,
         datasets_folder: Path,
@@ -243,3 +242,34 @@ def count_rules(file: Path) -> int:
     with open(file, 'r') as f:
         amount = len(f.readlines())
     return amount
+
+
+def balanced_accuracy_score(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64 | np.ndarray:
+    """
+    Calculate the balanced accuracy score given the true labels and predicted labels.
+
+    Parameters:
+    y_true (array-like): The true labels.
+    y_pred (array-like): The predicted labels.
+
+    Returns:
+    float: The balanced accuracy score.
+    """
+    # Convert input to numpy arrays to ensure vectorized operations
+    classes = list(np.unique(np.append(y_true, y_pred)))
+    y_true_prime = np.array([classes.index(label) for label in y_true])
+    y_pred_prime = np.array([classes.index(label) for label in y_pred])
+
+    # Count the number of occurrences of each label in y_true
+    label_counts = np.bincount(y_true_prime)
+
+    # Calculate the number of occurrences of each label in y_pred for each true label
+    tp_counts = np.bincount(y_true_prime[y_pred_prime == y_true_prime], minlength=len(label_counts))
+
+    # Calculate the balanced accuracy
+    balanced_acc = np.mean(
+        np.divide(tp_counts, label_counts, out=np.zeros_like(tp_counts, dtype='float64'), where=label_counts != 0),
+        where=label_counts != 0
+    )
+
+    return balanced_acc
