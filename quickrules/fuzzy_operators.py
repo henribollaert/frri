2c5bdf897d9f04ef53a4cff40af29df904e87501
hebollae <Henri.Bollaert@UGent.be>
2023-05-22 13:31:09 +0200

    added lukasiewicz

--- quickrules/fuzzy_operators.py
+++ quickrules/fuzzy_operators.py
@@ -11,6 +11,14 @@ class MinTNorm:
         return "Minimum t-norm"
 
 
+class LukasiewiczTNorm:
+    def __call__(self, a, b):
+        return max(0, a + b - 1)
+
+    def __repr__(self):
+        return "Lukasiewicz t-norm"
+
+
 class KleeneDienesImplicator:
     def __call__(self, a, b):
         return max(1 - a, b)
