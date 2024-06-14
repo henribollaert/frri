bed35e5e4d32c943a823844fa4aefb1efc6a12a5
hebollae <Henri.Bollaert@UGent.be>
2024-06-14 13:22:29 +0200

    more gran approx

--- fuzzyroughrules/approximations.py
+++ fuzzyroughrules/approximations.py
@@ -33,3 +33,37 @@ class MulticlassMSECVXApproximation:
             nn_approx=self.nn_approx,
             n_jobs=self.n_jobs
         )
+
+
+@dataclass
+class MulticlassMAECVXApproximation:
+    weights: Optional[np.ndarray] = None
+    nn_approx: int = -1
+    n_jobs: Optional[int] = None
+
+    def get_approximation(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        rel_matrix_x = fo.triangular_similarity(X, X)
+        rel_matrix_y = fo.discernibility_matrix(y, y)
+        return fo.get_multiclass_granular_approx_mae_cvxopt(
+            relation_matrix_x=rel_matrix_x,
+            relation_matrix_y=rel_matrix_y,
+            weights=self.weights,
+            nn_approx=self.nn_approx,
+            n_jobs=self.n_jobs
+        )
+
+
+@dataclass
+class MulticlassMSEGurobiApproximation:
+    weights: Optional[np.ndarray] = None
+    nn_approx: int = -1
+
+    def get_approximation(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        rel_matrix_x = fo.triangular_similarity(X, X)
+        rel_matrix_y = fo.discernibility_matrix(y, y)
+        return fo.get_multiclass_granular_approx_mse(
+            relation_matrix_x=rel_matrix_x,
+            relation_matrix_y=rel_matrix_y,
+            weights=self.weights,
+            nn_approx=self.nn_approx,
+        )
